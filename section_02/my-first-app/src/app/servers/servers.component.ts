cce622b20547406d0957060c99280ae29b1e089a
Vinicius Linck <vinicius.linck@clevertech.biz>
2023-08-21 10:36:22 -0300

    section 02 - lesson 29

--- section_02/my-first-app/src/app/servers/servers.component.ts
+++ section_02/my-first-app/src/app/servers/servers.component.ts
@@ -14,6 +14,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
+  serverCreationStatus = 'No server was created';
+
 
   constructor() {
     setTimeout(()=> {
@@ -22,4 +24,8 @@ export class ServersComponent implements OnInit {
   }
 
   ngOnInit() {}
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created!';
+  }
 }
