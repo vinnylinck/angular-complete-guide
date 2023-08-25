722aa3cd8576299ee9f66e54d925b41c928ba893
Vinny Linck <vinnylinck@gmail.com>
2023-08-25 10:27:28 -0300

    section 02 - lesson 41

--- section_02/my-first-app/src/app/servers/servers.component.ts
+++ section_02/my-first-app/src/app/servers/servers.component.ts
@@ -16,6 +16,8 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created';
   serverName = 'Testserver';
+  serverCreated = false;
+  servers = ['Testeserver', 'Testserver 2'];
 
 
   constructor() {
@@ -28,6 +30,8 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
   }
 
   onUpdateServerName(event: Event) {
