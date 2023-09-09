ab5cc76c36ae8d7539355f07452b60ed9abb62f4
Vinny Linck <vinnylinck@gmail.com>
2023-09-09 19:00:48 -0300

    section 07 - lesson 95

--- section_07/directives-start/src/app/app.module.ts
+++ section_07/directives-start/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { BasicHighlightDiretive } from './basic-highlight/basic-highlight.directive';
+import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicHighlightDiretive,
+    BetterHighlightDirective
   ],
   imports: [
     BrowserModule,
