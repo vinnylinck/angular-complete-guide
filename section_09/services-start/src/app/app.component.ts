8c29a185ac9fb16d4418f469c374f82e13ed45ec
Vinny Linck <vinnylinck@gmail.com>
2023-09-13 12:02:40 -0300

    section 09 - lesson 112

--- section_09/services-start/src/app/app.component.ts
+++ section_09/services-start/src/app/app.component.ts
@@ -1,25 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from './account.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: []
 })
-export class AppComponent {
-  accounts = [
-    {
-      name: 'Master Account',
-      status: 'active'
-    },
-    {
-      name: 'Testaccount',
-      status: 'inactive'
-    },
-    {
-      name: 'Hidden Account',
-      status: 'unknown'
-    }
-  ];
+export class AppComponent implements OnInit {
+  accounts: {name: string, status: string}[] = [];
+
+  constructor(private accountService: AccountService){}
+
+  ngOnInit(): void {
+    this.accounts = this.accountService.accounts;
+  }
 
   onAccountAdded(newAccount: {name: string, status: string}) {
     this.accounts.push(newAccount);
