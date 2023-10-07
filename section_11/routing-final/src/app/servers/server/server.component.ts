78c7f2f5ecc1bec265d029d3cff477c0df320b70
Vinny Linck <vinnylinck@gmail.com>
2023-10-07 13:26:26 -0300

    section 11 - end

--- section_11/routing-final/src/app/servers/server/server.component.ts
+++ section_11/routing-final/src/app/servers/server/server.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router, Data } from '@angular/router';
 
 import { ServersService } from '../servers.service';
-import { ActivatedRoute, Data, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-server',
@@ -12,15 +12,17 @@ export class ServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
 
   constructor(private serversService: ServersService,
-      private route: ActivatedRoute,
-      private router: Router) { }
+              private route: ActivatedRoute,
+              private router: Router) {
+  }
 
   ngOnInit() {
-    this.route.data.subscribe(
-      (data: Data) => {
-        this.server = data['server'];
-      }
-    );
+    this.route.data
+      .subscribe(
+        (data: Data) => {
+          this.server = data['server'];
+        }
+      );
     // const id = +this.route.snapshot.params['id'];
     // this.server = this.serversService.getServer(id);
     // this.route.params
@@ -31,8 +33,8 @@ export class ServerComponent implements OnInit {
     //   );
   }
 
-  onEdit(){
-    this.router.navigate(['edit'], {relativeTo: this.route,queryParamsHandling: 'preserve'})
+  onEdit() {
+    this.router.navigate(['edit'], {relativeTo: this.route, queryParamsHandling: 'preserve'});
   }
 
 }
