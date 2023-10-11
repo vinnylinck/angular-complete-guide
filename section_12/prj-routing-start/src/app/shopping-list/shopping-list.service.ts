9df12e8cede9d6f73789ca698793d98ad4c15db1
Vinny Linck <vinnylinck@gmail.com>
2023-10-11 13:24:06 -0300

    section 12 - lesson 179

---
+++ section_12/prj-routing-start/src/app/shopping-list/shopping-list.service.ts
@@ -0,0 +1,27 @@
+import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
+
+export class ShoppingListService {
+  ingredientChanged = new EventEmitter<Ingredient[]>();
+  private ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10),
+  ];
+
+  getIngredients() {
+    return this.ingredients.slice();
+  }
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    this.ingredientChanged.emit(this.getIngredients());
+  }
+
+  addIngredients(ingredients: Ingredient[]) {
+    // for (let ingredient of ingredients) {
+    //   this.addIngredients
+    // }
+    this.ingredients.push(...ingredients);
+    this.ingredientChanged.emit(this.getIngredients());
+  }
+}
