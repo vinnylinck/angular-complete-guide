cce232b2aa01cb196ee72567c0826c7b922876fb
Vinny Linck <vinnylinck@gmail.com>
2023-10-12 13:25:55 -0300

    section 12 - end

--- section_12/prj-routing-start/src/app/recipes/recipe.service.ts
+++ section_12/prj-routing-start/src/app/recipes/recipe.service.ts
@@ -33,6 +33,10 @@ export class RecipeService {
     return this.recipes.slice(); // returns an array copy
   }
 
+  getRecipe(index: number) {
+    return this.recipes[index];
+  }
+
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
     this.slService.addIngredients(ingredients);
   }
